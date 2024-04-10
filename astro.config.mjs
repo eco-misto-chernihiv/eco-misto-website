bfaa213f8540f8b9ebc76d957ca7fc8f76474335
Vladislav Kovac <vladkovac@Vladislavs-iMac.Dlink>
2024-04-10 20:56:37 +0300

    fix: Various fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import react from "@astrojs/react";
 
+// https://github.com/codiume/orbit/tree/main/packages/astro-purgecss
+// If using SSR, purgecss should be tested and configured
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react()]
+  integrations: [icon(), react(), purgecss()],
 });
