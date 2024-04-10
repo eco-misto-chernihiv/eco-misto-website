9f0640bdd2b5bb2aba8de3bfdc42ea00c03ef619
Vladislav Kovac <vladkovac@Vladislavs-iMac.Dlink>
2024-04-10 13:17:42 +0300

    feat: Add head component

---
+++ src/data/siteData.ts
@@ -0,0 +1,4 @@
+export const baseData = {
+    title: 'Eco-misto',
+    description: 'Ecomisto is a non-profit social organization dedicated to promoting environmental sustainability and eco-friendly practices within local communities.'
+}
