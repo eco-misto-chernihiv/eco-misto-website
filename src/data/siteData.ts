b3e7963b676c52f7b2eb89bd984e3b9ed3e969c5
Vladislav Kovac <vladkovac@Vladislavs-iMac.Dlink>
2024-05-07 09:47:14 +0300

    feat: Add initial footer

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -7,7 +7,7 @@ export const baseData = {
 export const navData = [
   {
     title: "Головна",
-    href: "#",
+    href: "/",
   },
   {
     title: "Проєкти",
@@ -15,6 +15,45 @@ export const navData = [
   },
   {
     title: "Контакти",
+    href: "#contacts",
+  },
+];
+
+export const socialData = [
+  {
+    title: "Instagram",
+    href: "https://www.instagram.com/eco_misto",
+    icon: "icon-instagram",
+  },
+  {
+    title: "Facebook",
+    href: "https://www.facebook.com/eco.cn.ua",
+    icon: "icon-facebook",
+  },
+  {
+    title: "Youtube",
+    href: "https://www.youtube.com/@eco-misto",
+    icon: "icon-youtube",
+  },
+  {
+    title: "LinkedIn",
     href: "#",
+    icon: "icon-linkedin",
+  },
+];
+
+export const contactsData = [
+  {
+    description: "+3 067 000 44 33",
+    icon: "icon-phone",
+  },
+  {
+    description: "hello@ecomisto.com",
+    icon: "icon-mail",
+  },
+  {
+    description:
+      "вулиця Шевченка, 95, Чернігів, Чернігівська область, Україна, 14027",
+    icon: "icon-pin",
   },
 ];
