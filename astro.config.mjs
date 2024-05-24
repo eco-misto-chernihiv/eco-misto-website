9da312e205afec9b7c5495c16373c19419dba4f3
Vladislav Kovac <vladkovac@Vladislavs-iMac.Dlink>
2024-05-24 13:35:26 +0300

    feat: Add arrow buttons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
-import icon from "astro-icon";
 import react from "@astrojs/react";
 
 // https://github.com/codiume/orbit/tree/main/packages/astro-purgecss
 // If using SSR, purgecss should be tested and configured
 import purgecss from "astro-purgecss";
 import netlify from "@astrojs/netlify";
-
 import mdx from "@astrojs/mdx";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react(), purgecss(), mdx()],
+  integrations: [react(), purgecss(), mdx(), icon()],
   devToolbar: {
     enabled: false
   },
