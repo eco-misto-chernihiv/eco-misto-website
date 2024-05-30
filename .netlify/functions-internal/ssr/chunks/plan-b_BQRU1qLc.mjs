9af1aaa415280815ba6238b9a78cf5b11a824a92
Vladislav Kovac <vladkovac@Vladislavs-iMac.Dlink>
2024-05-30 12:03:15 +0300

    feat: Add youtube video component into project page

--- .netlify/functions-internal/ssr/chunks/plan-b_BQRU1qLc.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./plan-b_e5jT3W6n.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
