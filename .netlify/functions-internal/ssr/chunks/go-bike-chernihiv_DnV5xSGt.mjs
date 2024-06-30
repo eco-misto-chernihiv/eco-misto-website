3b21ff378aae5e22e6852dfb88ce544608b0ede6
Vladislav Kovac <vladkovac@Vladislavs-iMac.Dlink>
2024-06-30 15:10:34 +0300

    feat: Cosmetic changes

--- .netlify/functions-internal/ssr/chunks/go-bike-chernihiv_DnV5xSGt.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./go-bike-chernihiv_B4Vq221J.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- .netlify/functions-internal/ssr/chunks/plastic-fantastic_CTzt06Ea.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./plastic-fantastic_D2KHkmHg.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [".frame{align-items:center;aspect-ratio:var(--left) /var(--right);display:flex;justify-content:center;overflow:hidden}.frame>img,.frame>picture,.frame>picture>img,.frame>video{height:100%;-o-object-fit:cover;object-fit:cover;width:100%}.frame>iframe{height:100%;width:100%}\n",".row[data-astro-cid-l73y7qbm]{display:grid;grid-template-columns:repeat(12,1fr);grid-template-rows:1fr auto}\n"];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
