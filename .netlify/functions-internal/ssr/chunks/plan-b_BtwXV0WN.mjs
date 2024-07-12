0a26eba7b9f29201e2ced8fe8a1c62c2b5382611
Vladislav Kovac <vladkovac@Vladislavs-iMac.Dlink>
2024-07-12 17:03:19 +0300

    feat: Add Prostir Diy content

--- .netlify/functions-internal/ssr/chunks/plan-b_BtwXV0WN.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./plan-b_CI8sa6Rl.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- .netlify/functions-internal/ssr/chunks/velolink_DReS1YNr.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./velolink_D-OS-Sjk.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
