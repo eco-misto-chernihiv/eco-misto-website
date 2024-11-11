5edbe811f8bef525157b5c1c369ba204dbc91584
Vladislav Kovac <vladkovac@Vladislavs-iMac.Dlink>
2024-11-11 10:25:44 +0200

    feat: Add favicons

---
+++ src/pages/manifest.json.ts
@@ -0,0 +1,39 @@
+// Server rendered
+export const prerender = false;
+
+import type { APIRoute } from "astro";
+import { getImage } from "astro:assets";
+import favicon from "../assets/favicon.png";
+import { baseData } from "@/data/siteData";
+
+const faviconPngSizes = [192, 512];
+
+export const GET: APIRoute = async () => {
+  const icons = await Promise.all(
+    faviconPngSizes.map(async (size) => {
+      const image = await getImage({
+        src: favicon,
+        width: size,
+        height: size,
+        format: "png",
+      });
+
+      return {
+        src: image.src,
+        type: `image/${image.options.format}`,
+        sizes: `${image.options.width}x${image.options.height}`,
+      };
+    })
+  );
+
+  const manifest = {
+    name: baseData.title,
+    description: baseData.description,
+    start_url: "/",
+    display: "standalone",
+    id: "ecomisto-id",
+    icons,
+  };
+
+  return new Response(JSON.stringify(manifest));
+};
